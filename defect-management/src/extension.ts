61b6a9f9117ab906b1a41dc1971002fc13fed890
Chen30lv <2444973096@qq.com>
2023-10-17 19:48:12 +0800

    Interface and buttons completed on the treeView

--- defect-management/src/extension.ts
+++ defect-management/src/extension.ts
@@ -13,6 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "defect-management" is now active!');
 	require('./sidebar')(context)
+	require('./views/count')(context)
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
