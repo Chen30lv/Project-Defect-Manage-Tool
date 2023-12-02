afeb209b3a3c0eb1076d2c7c6535f57f6de12303
Ding Yi <dw520521@gmail.com>
2023-12-02 10:44:44 +0800

    StatisticControllerTest

--- Service/defect/src/test/java/com/cityu/defect/controller/StatisticControllerTests.java
+++ Service/defect/src/test/java/com/cityu/defect/controller/StatisticControllerTests.java
@@ -1,13 +1,26 @@
 package com.cityu.defect.controller;
 
+import com.cityu.defect.model.dto.statisticInfo.StatisticQueryRequest;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootTest
 class StatisticControllerTests {
-//    @Test
-//    void test(){
-//        DefectInfoVO defectInfoVO = new DefectInfoVO();
-//        System.out.println(StringUtils.isBlank(defectInfoVO.getDefectLevel()));
-//    }
+    @Resource
+    StatisticController statisticController;
+    @Test
+    void test(){
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+        StatisticQueryRequest statisticQueryRequest = new StatisticQueryRequest();
+        statisticQueryRequest.setKey("project");
+        statisticQueryRequest.setUserId(1L);
+        System.out.println(statisticController.list(statisticQueryRequest,request));
+    }
 
 }
