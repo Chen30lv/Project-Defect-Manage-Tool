01cd8741396ddc6595db89e79d856ab1264187ee
QWERTYisme <Roywangjiahe@gmail.com>
2023-12-02 13:48:17 +0800

    fix the bug for popup

--- defect-management/src/views/dashboard.ts
+++ defect-management/src/views/dashboard.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import axios, { AxiosInstance } from 'axios';
 import { GlobalState, ProjectStats } from '../utils/globalState';
+import * as moment from 'moment-timezone';
 
 const path = require('path');
 const fs = require('fs');
@@ -108,9 +109,7 @@ async function fetchData(context: vscode.ExtensionContext): Promise<void> {
 		// 你的POST数据
 	  });
   
-    console.log('Response:', response.data);
     GlobalState.defectInfoArray = response.data.data;
-    console.log(GlobalState.defectInfoArray);
 
 	} catch (error) {
 	  console.error('Error during data posting:', error);
@@ -133,9 +132,7 @@ async function modifyStatus(context: vscode.ExtensionContext, item: SideBarEntry
 	  // 使用httpClient进行POST请求
 	  const response = await httpClient.post('/api/defectInfo/update', data);
   
-    console.log('Response:', response.data);
     GlobalState.defectInfoArray = response.data.data;
-    console.log(GlobalState.defectInfoArray);
 
 	} catch (error) {
 	  console.error('Error during data posting:', error);
@@ -160,9 +157,7 @@ async function addComments(context: vscode.ExtensionContext, item: SideBarEntryI
 	  // 使用httpClient进行POST请求
 	  const response = await httpClient.post('/api/defectInfo/update', data);
   
-    console.log('Response:', response.data);
     GlobalState.defectInfoArray = response.data.data;
-    console.log(GlobalState.defectInfoArray);
 
 	} catch (error) {
 	  console.error('Error during data posting:', error);
@@ -206,7 +201,7 @@ export class SideBarEntryItem extends vscode.TreeItem {
       tooltipContent.appendMarkdown(`**Defect Detail:**\n${defectDetail}\n\n`);
       tooltipContent.appendMarkdown(`---\n\n`);
       tooltipContent.appendMarkdown(`**Defect Comments**\n\n`);
-      console.log(defectComments);
+
       defectComments.forEach(comment => {
         tooltipContent.appendMarkdown(`---\n\n`);
         tooltipContent.appendMarkdown(`**Comment:** ${comment}\n\n`);
@@ -244,7 +239,6 @@ export class SideBarGeneric implements vscode.TreeDataProvider<SideBarEntryItem>
       // Child nodes
       var childrenList = [];
       for (let index = 0; index < GlobalState.defectInfoArray.length; index++) {
-        //console.log(GlobalState.defectInfoArray[index].isToDo)
         
         if (GlobalState.defectInfoArray[index].isToDo == element.label) {
           let emoji: string = selectEmoji(GlobalState.defectInfoArray[index].defectLevel);
@@ -311,7 +305,6 @@ export class SideBarGeneric implements vscode.TreeDataProvider<SideBarEntryItem>
 
 function registerDashboardCommands(context: vscode.ExtensionContext ,sidebar: SideBarGeneric) {
   context.subscriptions.push(vscode.commands.registerCommand('dashboard.markAsFixed', async (item: SideBarEntryItem)  => {
-    console.log("Start posting");
     await modifyStatus(context, item, "Fixed");
     await fetchData(context);
     sidebar.refresh();
@@ -319,7 +312,6 @@ function registerDashboardCommands(context: vscode.ExtensionContext ,sidebar: Si
   }));
 
   context.subscriptions.push(vscode.commands.registerCommand('dashboard.markAsDeferred', async (item) => {
-    console.log("Start posting");
     await modifyStatus(context, item, "Deferred");
     await fetchData(context);
     sidebar.refresh();
@@ -327,7 +319,6 @@ function registerDashboardCommands(context: vscode.ExtensionContext ,sidebar: Si
   }));
 
   context.subscriptions.push(vscode.commands.registerCommand('dashboard.markAsNotABug', async (item) => {
-    console.log("Start posting");
     await modifyStatus(context, item, "NotABug");
     await fetchData(context);
     sidebar.refresh();
@@ -335,7 +326,6 @@ function registerDashboardCommands(context: vscode.ExtensionContext ,sidebar: Si
   }));
 
   context.subscriptions.push(vscode.commands.registerCommand('dashboard.markAsDuplicate', async (item) => {
-    console.log("Start posting");
     await modifyStatus(context, item, "Duplicate");
     await fetchData(context);
     sidebar.refresh();
@@ -372,6 +362,32 @@ function registerDashboardCommands(context: vscode.ExtensionContext ,sidebar: Si
   }));
 }
 
+export function showPopup(context: vscode.ExtensionContext) {
+  let fix = 0;
+  let todo = 0;
+  let lastPopupDate = context.globalState.get('lastPopupDate');
+  console.log(lastPopupDate);
+  const now = moment().tz("Asia/Hong_kong");
+  const today = now.format('YYYY-MM-DD');
+  console.log(today);
+  if (lastPopupDate == today) {
+    let content = ' ';
+    GlobalState.defectInfoArray.forEach(defect => {
+      if(defect.defectStatus == 'Fixed'){
+        fix += 1;
+      }
+      else{
+        todo +=1;
+      }
+      content = `Daily statistics: `;
+      content += `[ Todo: ${todo}, Finished: ${fix} ]`;
+    });
+    vscode.window.showInformationMessage(content);
+    context.globalState.update('lastPopupDate', today);
+  }
+
+}
+
 
 module.exports = async function (context: vscode.ExtensionContext) {
   // Register Sidebar Panels
@@ -381,15 +397,14 @@ module.exports = async function (context: vscode.ExtensionContext) {
   vscode.window.registerTreeDataProvider('dashboard', sidebar);
   
   vscode.commands.registerCommand('dashboard.refresh', async () => {
-    console.log("Start posting");
     await fetchData(context);
     sidebar.refresh();
   });
 
   registerDashboardCommands(context, sidebar);
 
-  console.log("Start posting");
   await fetchData(context);
   sidebar.refresh();
+  showPopup(context);
   
 };
